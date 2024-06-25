24483877fb9487354716a073b6233d91472fa77a
Anderson Guerrero <adguerreroc@unal.edu.co>
2024-06-25 16:53:03 -0500

    first change complete

--- codes_hackerRank/codes_c/counter.c
+++ codes_hackerRank/codes_c/counter.c
@@ -5,30 +5,43 @@
 
 int main(){
 	int n, m;
+	printf("Ingrese un numero entre 10000 y 99999 \n");
 	scanf("%d", &n);
 
 	int aux=n;
 	for(int i=0; i<5; i++){
 	int aux2;
-	if(aux/10>=1000){
-		aux2=(aux/10)/1000;
-		aux=aux%10;
-		printf("%d %d", aux2, aux);
+	if(aux>=10000 && aux<100000){
+		aux2=aux/10000;
+		aux=aux%10000;
+		printf("%d-%d \n", aux2, aux);
 	}
-	else if(aux/10>=100 && aux/10<1000){
-		aux2=aux2+(aux/10)/100;
-		aux=aux%10;
-		printf("%d %d", aux2, aux);
+	else if(aux>=1000 && aux<10000){
+		aux2=aux2+aux/1000;
+		aux=aux%1000;
+		printf("%d-%d \n", aux2, aux);
 	}
-	else if(aux/10>=10 && aux<100){
-		aux2=aux2+(aux/10)/100;
+	else if(aux>=100 && aux<1000){
+		aux2=aux2+aux/100;
+		aux=aux%100;
+		printf("%d-%d \n", aux2, aux);
+	}
+	else if(aux>=10 && aux<100){
+		aux2=aux2+aux/10;
 		aux=aux%10;
-		printf("%d %d", aux2, aux);
+		printf("%d-%d \n", aux2, aux);
+	}
+	else if(aux<10){
+		aux2=aux2+aux;
+		aux=aux/10;
+		printf("%d-%d \n", aux2, aux);
 	}
 	else{
-		aux2=aux2+aux/10+ aux%10;
+		aux2=aux2+aux;
 		printf("%d",aux2);
 	}
+	m=aux2;
 	}
+	printf("The result is:%d \n", m);
 	return 0;
 }
