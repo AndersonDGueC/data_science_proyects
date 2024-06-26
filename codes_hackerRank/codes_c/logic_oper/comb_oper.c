fb54c84892cf604c35947f7a2e1c1a42957027bd
Anderson Guerrero <adguerreroc@unal.edu.co>
2024-06-26 13:36:37 -0500

    phase var aux finally

--- codes_hackerRank/codes_c/logic_oper/comb_oper.c
+++ codes_hackerRank/codes_c/logic_oper/comb_oper.c
@@ -12,12 +12,21 @@
 #include <stdlib.h>
 
 void calculate_the_maximum(int n, int k){
+	int aux1=0;
+	int aux2=0; 
+	int aux3=0;
 	if(n>=k && n>=2){
 	printf("good ;) \n");
-	for(int i=1; i<=n; i++){
+	for(int i=1; i<=n; i++){	
 		for(int j=1+i; j<=n; j++){
 		if(i!=(j+1) && i<(j+1)){
-		printf("%d %d  \n", i, j);
+		    printf("%d %d %d %d %d \n", i, j, i&j, i|j, i^j);
+			if(aux1>(i&j) && aux1<k){
+					aux1=i&j;
+					}
+			else{
+				aux1=aux1;
+				}		
 				}
 			}
 		}
@@ -25,6 +34,7 @@ void calculate_the_maximum(int n, int k){
 	else{
 	printf("WRONG, n has greather or equal than k and n greather than 2");
 	}
+	printf("%d", aux1);
 }
 
 int main(){
