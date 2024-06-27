1d47fce5b4443a0e0b148833eb290b41d367d352
Anderson Guerrero <adguerreroc@unal.edu.co>
2024-06-27 08:31:16 -0500

    finally coding logic operation or bitwise

--- codes_hackerRank/codes_c/logic_oper/comb_oper.c
+++ codes_hackerRank/codes_c/logic_oper/comb_oper.c
@@ -20,26 +20,33 @@ void calculate_the_maximum(int n, int k){
 	for(int i=1; i<=n; i++){	
 		for(int j=1+i; j<=n; j++){
 		if(i!=(j+1) && i<(j+1)){
-		    printf("%d %d %d %d %d \n", i, j, i&j, i|j, i^j);
-			if(aux1>(i&j) && aux1<k){
-					aux1=i&j;
-					}
-			else{
-				aux1=aux1;
-				}		
-				}
+		    //printf("%d %d %d %d %d \n", i, j, i&j, i|j, i^j);
+		   	
+           if((i&j)>=aux1 && (i&j)<k){
+              aux1=i&j;
+              printf("%d %d \n", i&j, k);
+            }
+           if((i|j)>=aux2 && (i|j)<k){
+              aux2=i|j;
+              printf("%d %d \n", i|j, k);
+           }
+           if((i^j)>=aux3 && (i^j)<k){
+		      aux3=i^j;
+              printf("%d %d \n", i^j, k);
+           }
 			}
 		}
-	}
-	else{
+    }
+    }
+    else{
 	printf("WRONG, n has greather or equal than k and n greather than 2");
 	}
-	printf("%d", aux1);
+	printf("%d\n%d\n%d\n", aux1, aux2, aux3);
 }
 
 int main(){
 	int n, k;
 
-	scanf("%d &d", &n, &k);
+	scanf("%d %d", &n, &k);
 	calculate_the_maximum(n, k);
 }
