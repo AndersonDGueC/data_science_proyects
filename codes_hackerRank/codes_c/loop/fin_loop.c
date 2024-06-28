2fc3d624ffe375a4e1b4e2bef828d7b3c5957217
Anderson Guerrero <adguerreroc@unal.edu.co>
2024-06-28 10:34:46 -0500

    first part complete loop

--- codes_hackerRank/codes_c/loop/fin_loop.c
+++ codes_hackerRank/codes_c/loop/fin_loop.c
@@ -18,7 +18,7 @@ int main()
     aux=n;
     for(int i=0; i<(n+(n-1)); i++){
       for(int j=0; j<(n+(n-1)); j++){
-          if(j>=i && j<=(n+(n-1))-i){
+          if(j>=i && j<(n+(n-1))-i){
           printf("%d",aux);
           }
           else{
