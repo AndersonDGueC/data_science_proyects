c03465df03840105f6ebda82e1f4b50b3b5b0a06
Anderson Guerrero <adguerreroc@unal.edu.co>
2024-07-22 18:18:03 -0500

    array 1D in C, finally

---
+++ codes_hackerRank/codes_c/array_dynamic/array_din.c
@@ -0,0 +1,31 @@
+///////////////////////////////////
+//////////////////////////////////
+//////////////atgreenbay//////////
+//////////////////////////////////
+//////////////////////////////////
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+int n;
+int sum=0;
+scanf("%d", &n);
+int *arr=(int*)malloc(n*sizeof(int));
+
+for(int i=0; i<n; i++){
+scanf("%d", &arr[i]);
+}
+
+for (int j=0; j<n; j++){
+sum+=arr[j];
+}
+
+printf("%d", sum);
+free(arr);
+
+return 0;
+}
+
